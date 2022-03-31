aca4c9c2b4a22b9d1b1739f7485db4dcf2ca719b
Mor Shemesh <morshe@wix.com>
2022-03-31 12:39:58 +0300

    update handshake

--- src/iframeHandshake.ts
+++ src/iframeHandshake.ts
@@ -3,13 +3,13 @@ import { ChannelPickerMessageType, EstablishConnectionMessage } from './types';
 export function iframeHandshake(bridgeType: string, version: string): Promise<MessagePort> {
   return new Promise(resolve => {
     const { port1, port2 } = new MessageChannel();
-    window.parent.postMessage({ type: ChannelPickerMessageType, bridgeType, version }, '*', [port1]);
     const initialize = (event: MessageEvent<typeof EstablishConnectionMessage>) => {
       if (event.data.comlinkInit) {
-        port2.removeEventListener('message', initialize);
-        resolve(port2);
+        port1.onmessage = null;
+        resolve(port1);
       }
     };
-    port2.addEventListener("message", initialize);
+    port1.onmessage = initialize;
+    window.parent.postMessage({ type: ChannelPickerMessageType, bridgeType, version }, '*', [port2]);
   });
 }
